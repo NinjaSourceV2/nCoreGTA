174d3c76784c2adb270ba160ff1060d53060123c
RamexDeltaXOO <walid.bko47@gmail.com>
2021-02-11 09:54:21 +0100

    Changement récupération license

--- resources/[GTA_Main]/nMainFood/server.lua
+++ resources/[GTA_Main]/nMainFood/server.lua
@@ -1,7 +1,13 @@
 RegisterServerEvent('nGetStats')
 AddEventHandler('nGetStats', function()
 	local source = source
-	local license = GetPlayerIdentifiers(source)[1]
+	local license = ""
+        local Identifiers = GetPlayerIdentifiers(source)
+        for i,identifier in ipairs(Identifiers) do
+            if string.find(identifier, "license:") then
+                license = identifier
+            end
+        end
 
 	MySQL.Async.fetchAll('SELECT * FROM gta_joueurs WHERE license = @license',{['@license'] = license}, function(res)
 		TriggerClientEvent('nGetStats', source, res[1].faim, res[1].soif)
@@ -11,13 +17,25 @@ end)
 RegisterServerEvent("nSetFaim")
 AddEventHandler("nSetFaim", function(faim)
 	local source = source
-	local license = GetPlayerIdentifiers(source)[1]
+	local license = ""
+        local Identifiers = GetPlayerIdentifiers(source)
+        for i,identifier in ipairs(Identifiers) do
+            if string.find(identifier, "license:") then
+                license = identifier
+            end
+        end
 	MySQL.Async.execute("UPDATE gta_joueurs SET faim=@faim WHERE license=@license", { ['@license'] = license, ['@faim'] = tostring(faim)})
 end)
 
 RegisterServerEvent("nSetSoif")
 AddEventHandler("nSetSoif", function(soif)
 	local source = source
-	local license = GetPlayerIdentifiers(source)[1]
+	local license = ""
+        local Identifiers = GetPlayerIdentifiers(source)
+        for i,identifier in ipairs(Identifiers) do
+            if string.find(identifier, "license:") then
+                license = identifier
+            end
+        end
 	MySQL.Async.execute("UPDATE gta_joueurs SET soif=@soif WHERE license=@license", { ['@license'] = license, ['@soif'] = tostring(soif)})
 end)
