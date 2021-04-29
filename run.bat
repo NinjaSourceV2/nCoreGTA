7ff43275a9b7e5d16f8c4d117f8e7eb7c59a2c68
Ninja <samir932205@gmail.com>
2021-04-29 13:45:33 +0200

    onesync_enableInfinity added

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 rmdir /s /q cache
 TIMEOUT /T 1
-start FXServer.exe +exec server.cfg +set onesync on +set onesync legacy +set svgui_disable true +set sv_projectName "Source Ready" "sample"
+start FXServer.exe +exec server.cfg +set onesync on +set onesync legacy +set onesync_enableInfinity 1 +set svgui_disable true +set sv_projectName "Source Ready" "sample"
